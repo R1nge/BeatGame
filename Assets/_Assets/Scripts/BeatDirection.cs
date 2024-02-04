39a6d0658fbe0d7e7efaf885a741b84f594081e3
R1nge <r1ngegamedev@gmail.com>
2024-02-04 14:22:33 +0300

    backup

--- Assets/_Assets/Scripts/BeatDirection.cs
+++ Assets/_Assets/Scripts/BeatDirection.cs
@@ -4,17 +4,27 @@ namespace _Assets.Scripts
 {
     public class BeatDirection : MonoBehaviour
     {
+        [SerializeField] private int amountOfBeatsToSkip;
         private bool _beatDetected = false;
+        private int _skippedBeats;
+
         private void Awake()
         {
             AudioProcessor processor = FindObjectOfType<AudioProcessor>();
             processor.onBeat.AddListener(BeatDetected);
             processor.onSpectrum.AddListener(SpectrumChanged);
         }
-        
+
         private void BeatDetected()
         {
+            if (_skippedBeats < amountOfBeatsToSkip)
+            {
+                _skippedBeats++;
+                return;
+            }
+
             _beatDetected = true;
+            _skippedBeats = 0;
         }
 
         private void SpectrumChanged(float[] spectrum)
@@ -35,20 +45,19 @@ namespace _Assets.Scripts
                         Debug.Log("right");
                         break;
                     }
-                    
-                    if(spectrum[6] > 0.5 || spectrum[7] > 0.5 || spectrum[8] > 0.5)
+
+                    if (spectrum[6] > 0.5 || spectrum[7] > 0.5 || spectrum[8] > 0.5)
                     {
                         Debug.Log("up");
                         break;
                     }
-                    
-                    if(spectrum[9] > 0.5 || spectrum[10] > 0.5 || spectrum[11] > 0.5)
+
+                    if (spectrum[9] > 0.5 || spectrum[10] > 0.5 || spectrum[11] > 0.5)
                     {
                         Debug.Log("down");
                         break;
                     }
-                    
-                }   
+                }
             }
         }
     }
---
+++ Assets/todo.md
@@ -0,0 +1,6 @@
+﻿//TODO:
+////Read the spectrogram before hand, then parse it from json and use?
+////Need to read samples from an audio (NAudio)
+////Then divide sample into ranges (bands)
+////44Khz, 12 bands, 2Khz per band
+////44Khz, 4 bands, 11Khz per band
